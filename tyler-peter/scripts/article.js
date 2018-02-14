9cd52cd7487f8d635c5ea9ee21a0594498dc6bf4
Peter Kim <seattlechem@yahoo.com>
2018-02-14 12:17:38 -0800

    completed prototype .toHTML function and created README.md

--- tyler-peter/scripts/article.js
+++ tyler-peter/scripts/article.js
@@ -10,7 +10,7 @@ function Article (rawDataObj) {
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
   this.author = rawDataObj.author;
-  this.authorURL = rawDataObj.authorURL;
+  this.authorUrl = rawDataObj.authorUrl;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
   // done: Save ALL the properties of `rawDataObj` into `this`
@@ -24,21 +24,16 @@ Article.prototype.toHtml = function() {
   /* done: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
-
+  $newArticle.removeClass('template');
   $newArticle.attr('data-category', this.category);
-  console.log(this.title);
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
-    We need to fill in:
-        find correct element
-        set html value to ______
-      1. author name,
-      2. author url,
-      3. article title,
 
-        select the h1
-        add text content into it that is this.title
-      4. article body, and
-      5. publication date. */
+  // Done: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time').attr('datetime', this.publishedOn);
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -46,18 +41,17 @@ Article.prototype.toHtml = function() {
   return $newArticle;
 };
 
-// rawData.sort(function(a,b) {
-//   // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
-//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-// });
+rawData.sort(function(a,b) {
+  // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+rawData.forEach(function(val, index){
+  articles.push(new Article(rawData[index]));
+})
 
-for(let i = 0; i < articles.length; i++) {
-  // console.log('hit', i);
-  $('#articles').append(`<h1>${articles[i].title}</h1>`); //articles[i].toHtml()
-}
+articles.forEach(function(val, index){
+  $('#articles').append(articles[index].toHtml());
+})
