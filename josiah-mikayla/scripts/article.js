8c2d000c315bc9e4384a41e2f089586540990d9e
Josiah Green <JosiahGreen90@gmail.com>
2018-02-14 11:57:47 -0800

    worked on jQuery a bit

--- josiah-mikayla/scripts/article.js
+++ josiah-mikayla/scripts/article.js
@@ -18,11 +18,9 @@ function Article (rawDataObj) {
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // You clone an article (or any object) so you can modify it later without affecting the original content. 
-
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
-  $('')
+  /* DONE(?): This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
