5ef92f6d0025edcb11d5549d2ecd69af6ee15bca
zachary schumpert <buphnezz@gmail.com>
2018-02-24 22:12:53 -0800

    updating styling to reflect the finished project more accurately

--- haron-zachary/scripts/article.js
+++ haron-zachary/scripts/article.js
@@ -2,9 +2,8 @@
 
 let articles = [];
 
-// COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
-**save ALL the properties of 'rawDataObj' into 'this'**
+// DONE: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
+// We're saving all the properties of rawDataObj inside of our Article constructor so we can use .this to call those properties at a later time. Within the function, (rawDataObj) is a parameter where as rawDataObj.body is the actual body of text linked to the new article rawDataObj.
 
 function Article (rawDataObj) {
   this.title = rawDataObj.title;
@@ -20,7 +19,7 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // By cloning the article, we can simply copy and paste or appent the article later where need be, since we'll be using this article over and over again we want a clone to be made so we can modify that clone and then append it.  The original template stays in tact as a result of only cloning it.
   let $newArticle = $('article.template').clone();
   /* DONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
@@ -28,32 +27,22 @@ Article.prototype.toHtml = function() {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category); 
 
-$('p').append('name', 'this.author');
-
-  $newArticle.append('something');
-  console.log($newArticle);
-
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
       3. article title,
       4. article body, and
-      5. publication date. */
-  $(document).ready(function () {
-    $("body").append(this.articles[0]);
-    });
-    
-$newArticle.find('.byline a').html(this.author);
-$newArticle.find('.byline a').attr('href', this.authorUrl);
-$newArticle.find('h1:first').html(this.title);
-$newArticle.find('.article-body').html(this.body);
-$newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
-
-// REVIEW: Display the date as a relative number of 'days ago'
-$newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-$newArticle.append('<hr>');
-return $newArticle;
+      5. publication date. */    
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h1:first').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
+  // REVIEW: Display the date as a relative number of 'days ago'
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.append('<hr>');
+  return $newArticle;
 };
 
 rawData.sort(function(a,b) {
